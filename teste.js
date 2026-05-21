4aa19258e75a56a7ec03504ce60f456cf3f200b5
ViniciosVicente <viniciosanalyst9@gmail.com>
2026-05-21 09:09:34 -0300

    fix: Correções no loop de opções.

--- teste.js
+++ teste.js
@@ -2,21 +2,13 @@ const prompt = require('prompt-sync')();
 
 function sistema(){
    
-    let selecionarOpcoes = 0.
+    let selecionarOpcoes = 0;
 
     while(selecionarOpcoes != 5){
-        selecionarOpcoes = Number(
-            prompt(
-                "\n=== SISTEMA ===\n" +
-                "1 - CADASTRAR USUÁRIO\n" +
-                "2 - LISTAR USUÁRIO\n" +
-                "3 - MODIFICAR USUÁRIO\n" +
-                "4 - REMOVER USUÁRIO\n" +
-                "5 - FINALIZAR\n\n" +
-                "Escolha uma opção: "
+        selecionarOpcoes = Number (prompt("\n=== SISTEMA ===\n 1 - CADASTRAR USUÁRIO\n 2 - LISTAR USUÁRIO\n 3 - MODIFICAR USUÁRIO\n 4 - REMOVER USUÁRIO\n 5 - FINALIZAR\n:"));
     }
     
-
+    console.log("Sistema finalizado!")
 }
 
 sistema();
